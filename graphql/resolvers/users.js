6bb1acdc35f0ccb4afa36eeb82de67ec30128646
aftabumer <innocentboy0300@gmail.com>
2021-03-04 15:46:25 +0500

    -create user register mutation. -set user in database. -create login muatation. -craete field validation.

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,102 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
+
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../util/validators");
+const User = require("../../models/User");
+
+function generateToken(user) {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+}
+
+module.exports = {
+  Mutation: {
+    async login(_, { username, password }) {
+      const { valid, errors } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong crendential";
+        throw new UserInputError("Wrong crendential", { errors });
+      }
+
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } }
+    ) {
+      const { valid, errors } = validateRegisterInput(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is already taken", {
+          error: {
+            username: "This username is already taken",
+          },
+        });
+      }
+      const emailInput = await User.findOne({ email });
+      if (emailInput) {
+        throw new UserInputError("email is already taken", {
+          error: {
+            email: "This email is already taken",
+          },
+        });
+      }
+
+      password = await bcrypt.hash(password, 12);
+      const newUser = new User({
+        username,
+        password,
+        email,
+        createdAt: new Date().toISOString(),
+      });
+      const res = await newUser.save();
+
+      const token = generateToken(res);
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
