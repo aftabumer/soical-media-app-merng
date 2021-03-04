100dc3a6eee5c6b17d01c17beb5d70d3fa129ac0
aftabumer <innocentboy0300@gmail.com>
2021-03-04 18:59:41 +0500

    -create queries for getPosts and getPostById.

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -4,7 +4,7 @@ module.exports = gql`
   type Post {
     id: ID!
     body: String
-    userName: String
+    username: String
     createdAt: String
   }
   type User {
@@ -23,9 +23,12 @@ module.exports = gql`
 
   type Query {
     getPosts: [Post]
+    getPost(postId: ID!): Post
   }
   type Mutation {
     register(registerInput: RegisterInput): User!
     login(username: String!, password: String!): User!
+    createPost(body: String!): Post!
+    deletePost(postId: ID!): String!
   }
 `;
