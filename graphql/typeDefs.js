0bb70e452f8e55ee631cf962239e2a5b3d7f3516
aftabumer <innocentboy0300@gmail.com>
2021-03-05 21:47:48 +0500

    -create mutation for commentPost, likePost and deleteComment. -create subscription for getting new post.

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -3,9 +3,24 @@ const { gql } = require("apollo-server");
 module.exports = gql`
   type Post {
     id: ID!
-    body: String
-    username: String
-    createdAt: String
+    body: String!
+    username: String!
+    createdAt: String!
+    comments: [Comment]!
+    likes: [Like]!
+    commentCount: Int!
+    likeCount: Int!
+  }
+  type Comment {
+    id: ID!
+    createdAt: String!
+    username: String!
+    body: String!
+  }
+  type Like {
+    id: ID!
+    createdAt: String!
+    username: String!
   }
   type User {
     id: ID!
@@ -30,5 +45,11 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body: String!): Post!
     deletePost(postId: ID!): String!
+    createComment(postId: ID!, body: String!): Post!
+    deleteComment(postId: ID!, commentId: ID!): Post!
+    likePost(postId: ID!): Post!
+  }
+  type Subscription {
+    newPost: Post!
   }
 `;
