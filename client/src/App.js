825cea5141d8b92e2ac98053920c7e0ab539246c
aftabumer <innocentboy0300@gmail.com>
2021-03-07 19:10:28 +0500

    -implement sementic ui. -create routes for our application. -create header and menu bar.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+import { Home, Login, Register } from "./pages";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <div>
-      <h1>Hi World!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Container>
+    </Router>
   );
 }
 
