897b67c4430e207d5be6f8515684d79ed8be568e
aftabumer <innocentboy0300@gmail.com>
2021-03-08 20:30:40 +0500

    -change email errors.

--- util/validators.js
+++ util/validators.js
@@ -10,7 +10,7 @@ module.exports.validateRegisterInput = (
     errors.username = "Username must not be empty";
   }
   if (email.trim() === "") {
-    errors.email = "Username must not be empty";
+    errors.email = "Email must not be empty";
   } else {
     const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
     if (!email.match(regEx)) {
